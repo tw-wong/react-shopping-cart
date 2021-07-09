0e270c29eef712289c7d5fa299044cd01cdb38ec
tw-wong <wtw1982@yahoo.com>
2021-07-09 12:21:18 +0800

    lint-staged testing

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import ShoppingCart from './components/ShoppingCart'
 
+//Test lint-staged for ESLint
 const Home = () => {
   const config = {
     currency: 'RM',
